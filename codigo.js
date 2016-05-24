33a4ed44c64ad373a085f2d5a84cc88b94234f59
Yojan Pardo <yojan.pardo@gmail.com>
2016-05-24 13:28:08 -0500

    Archivos iniciales

---
+++ codigo.js
@@ -0,0 +1,9 @@
+function hacerClick() {
+	document.getElementsByTagName('p')[2].onclick=mostrarTexto();
+}
+
+function mostrarTexto() {
+	var texto=("ohh cyyy continua plox");
+	document.write("funciona?");
+	prueba.innerText=texto;
+}
